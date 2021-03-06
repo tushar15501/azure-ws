a3074ab9a7264b86ce2e455ccba8bd58af5d5bb3
Tushar Joshi <joshitushar210@gmail.com>
2021-03-06 15:02:52 +0530

    image

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Linux, World!"
+    return "Linux, World !! <b> Hello !! </b>  <img src = 'https://myimagestorage123.blob.core.windows.net/myconttest/1614679561595.jfif'>"
